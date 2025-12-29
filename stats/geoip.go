a456bc12a1435eca5069df608806cf3a521d8129
idk <hankhill19580@gmail.com>
2025-12-29 11:16:46 +0000

    Implement GeoIP struct and associated methods for IP lookup

--- stats/geoip.go
+++ stats/geoip.go
@@ -2,3 +2,17 @@ package stats
 
 // Simple GeoIP lookup tool using a local MaxMind DB.
 // Used to determine router locations from IP addresses in RouterInfo entries.
+
+type GeoIP struct {
+	DBPath string
+}
+
+func NewGeoIP(dbPath string) (GeoIP, error) {
+	return GeoIP{
+		DBPath: dbPath,
+	}, nil
+}
+
+func (g *GeoIP) Lookup(ip string) (string, error) {
+	return "Unknown", nil
+}
